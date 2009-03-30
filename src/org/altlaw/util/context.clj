128037df37b67609494535225bc6763bfd1b878a
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-30 16:54:02 -0400

    util/context.clj: simplified to use properties directly
    Don't do dynamic binding.  Exception on missing property.

--- src/org/altlaw/util/context.clj
+++ src/org/altlaw/util/context.clj
@@ -1,77 +1,34 @@
 (ns org.altlaw.util.context)
 
-(def #^{:private true} *altlaw-context*)
-
-(def #^{:private true}
-     *default-context*
-     {:home (System/getProperty "user.dir")
-      :env "development"})
-
-;; f is fn encapsulating a method call to some context object, that
-;; should return values for string keys.
-(defn- extract-context-with [f]
-  {:home (f "org.altlaw.home")
-   :env (f "org.altlaw.env")
-   :internal-uri (f "org.altlaw.internal.uri")})
-
-;; Extract context information from the given object.  May be a
-;; java.util.Properties, org.restlet.Context, or
-;; org.apache.hadoop.conf.Configuration.
-(defmulti #^{:private true} extract-context class)
-
-(defmethod extract-context java.util.Properties [props]
-  (extract-context-with #(.getProperty props %)))
-
-(defmethod extract-context org.restlet.Context [context]
-  (let [attrs (.getAttributes context)]
-    (extract-context-with #(.get attrs %))))
-
-(defmethod extract-context org.apache.hadoop.conf.Configuration [config]
-  (extract-context-with #(.get config %)))
+(defn get-property [name]
+  (or (System/getProperty name)
+      (throw (Exception. (str "Missing property " name)))))
 
 
 ;;; PUBLIC
 
-(defn setup-context
-  "Set up AltLaw's context object from the given context/configuration
-  object."
-  [obj]
-  (alter-var-root #'*altlaw-context*
-                  (fn [old] (merge *default-context* (extract-context obj)))))
-
 (defn altlaw-home
   "AltLaw's home directory, as String."
   []
-  (:home *altlaw-context*))
+  (get-property "org.altlaw.home"))
 
 (defn altlaw-env
   "AltLaw's running environment, a String.
   One of production/development/testing."
   []
-  (:env *altlaw-context*))
+  (get-property "org.altlaw.env"))
 
 (defn internal-uri
   "The base URI of the internal.altlaw.org server.  String, includes
   the scheme and host name, but not the initial slash."
   []
-  (:internal-uri *altlaw-context*))
+  (get-property "org.altlaw.internal.uri"))
 
 (defn internal-db
   "The DB description for clojure.contrib.sql/with-connection"
   []
   {:classname "org.apache.derby.jdbc.EmbeddedDriver"
    :subprotocol "derby"
-   :subname (str (altlaw-home) "/var/db/" (altlaw-env))
+   :subname (java.io.File. (str (altlaw-home) "/var/db/" (altlaw-env)))
    :create true})
 
-(defmacro with-altlaw-env
-  "Run body with a modified altlaw-env string."
-  [name & body] 
-  `(binding [*altlaw-context* (assoc *altlaw-context* :env name)]
-     ~@body))
-
-(defn testing-env-fixture
-  "Fixture function for use in tests.  Sets the altlaw-env to
-  'testing'."
-  [f]
-  (with-altlaw-env "testing" (f)))
