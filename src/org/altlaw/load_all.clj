4c0a2c04934ffad8505f4e84bbed32b2ef3a6e17
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-30 13:48:37 -0400

    load_all.clj: added file to load all libs in alphabetical order

---
+++ src/org/altlaw/load_all.clj
@@ -0,0 +1,12 @@
+(ns org.altlaw.load-all
+  (:require 
+   org.altlaw.test.run
+
+   org.altlaw.util.context
+   org.altlaw.util.date
+   org.altlaw.util.files
+   org.altlaw.util.hadoop
+   org.altlaw.util.lucene
+   org.altlaw.util.solr
+   org.altlaw.util.zip
+))
