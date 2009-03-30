ec57ac8c363721dffac5444c06b880961a1a05e5
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-30 14:10:10 -0400

    util/DateUtils.java: removed old copyright notice

--- src/org/altlaw/util/DateUtils.java
+++ src/org/altlaw/util/DateUtils.java
@@ -1,25 +1,3 @@
-/* 
- * This file is part of AltLaw, the free legal search engine.
- * 
- * Copyright (C) 2008 The Trustees of Columbia University
- * 
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * More information about this software is available at
- * <http://lawcommons.org/>.
- */
-
 package org.altlaw.util;
 
 import java.text.SimpleDateFormat;
