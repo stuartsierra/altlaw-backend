46cbd036a49da4d2d20774c5deca298d733faff3
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-30 14:08:25 -0400

    util/context.clj: added doc strings

--- src/org/altlaw/util/context.clj
+++ src/org/altlaw/util/context.clj
@@ -32,22 +32,38 @@
 
 ;;; PUBLIC
 
-(defn setup-context [obj]
+(defn setup-context
+  "Set up AltLaw's context object from the given context/configuration
+  object."
+  [obj]
   (alter-var-root #'*altlaw-context*
                   (fn [old] (merge *default-context* (extract-context obj)))))
 
-(defn altlaw-home []
+(defn altlaw-home
+  "AltLaw's home directory, as String."
+  []
   (:home *altlaw-context*))
 
-(defn altlaw-env []
+(defn altlaw-env
+  "AltLaw's running environment, a String.
+  One of production/development/testing."
+  []
   (:env *altlaw-context*))
 
-(defn altlaw-internal-uri []
+(defn altlaw-internal-uri
+  "The base URI of the internal.altlaw.org server.  String, includes
+  the scheme and host name, but not the initial slash."
+  []
   (:internal-uri *altlaw-context*))
 
-(defmacro with-altlaw-env [name & body] 
+(defmacro with-altlaw-env
+  "Run body with a modified altlaw-env string."
+  [name & body] 
   `(binding [*altlaw-context* (assoc *altlaw-context* :env name)]
      ~@body))
 
-(defn testing-env-fixture [f]
+(defn testing-env-fixture
+  "Fixture function for use in tests.  Sets the altlaw-env to
+  'testing'."
+  [f]
   (with-altlaw-env "testing" (f)))
