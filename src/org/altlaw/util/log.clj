3c0cbd2e7d4ab0a695658c1fa91a9ed74207e491
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-30 16:53:14 -0400

    util/log.clj: modified to use lexical namespace

--- src/org/altlaw/util/log.clj
+++ src/org/altlaw/util/log.clj
@@ -5,40 +5,35 @@
 
 (JavaLoggingToCommonLoggingRedirector/activate)
 
-;; Returns the logger named after the given namespace.  Caches
-;; loggers.
-(def get-ns-log
-     (memoize (fn [ns] (LogFactory/getLog (str (ns-name ns))))))
-
 (defmacro trace [label arg]
   `(let [value# ~arg
-         log# (get-ns-log *ns*)]
+         log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isTraceEnabled)
        (. log# (trace (str ~label ":" (pr-str value#)))))
      value#))
 
 (defmacro debug [& args]
-  `(let [log# (get-ns-log *ns*)]
+  `(let [log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isDebugEnabled)
        (. log# (debug (str ~@args))))))
 
 (defmacro info [& args]
-  `(let [log# (get-ns-log *ns*)]
+  `(let [log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isInfoEnabled)
        (. log# (info (str ~@args))))))
 
 (defmacro warn [& args]
-  `(let [log# (get-ns-log *ns*)]
+  `(let [log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isWarnEnabled)
        (. log# (warn (str ~@args))))))
 
 (defmacro error [& args]
-  `(let [log# (get-ns-log *ns*)]
+  `(let [log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isErrorEnabled)
        (. log# (error (str ~@args))))))
 
 (defmacro fatal [& args]
-  `(let [log# (get-ns-log *ns*)]
+  `(let [log# (. LogFactory getLog ~(str (ns-name *ns*)))]
      (when (. log# isFatalEnabled)
        (. log# (fatal (str ~@args))))))
 
