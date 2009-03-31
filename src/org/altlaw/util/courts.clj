c4ce9540674df07e9a66bac613ac5ba5f4aa6730
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-31 12:38:21 -0400

    util/courts.clj: copied from v4, for www/SearchResource.clj

---
+++ src/org/altlaw/util/courts.clj
@@ -0,0 +1,24 @@
+(ns org.altlaw.util.courts)
+
+(def *court-names* {
+"http://id.altlaw.org/courts/us/fed/supreme" "United States Supreme Court"
+"http://id.altlaw.org/courts/us/fed/app/1" "United States Court of Appeals for the First Circuit"
+"http://id.altlaw.org/courts/us/fed/app/2" "United States Court of Appeals for the Second Circuit"
+"http://id.altlaw.org/courts/us/fed/app/3" "United States Court of Appeals for the Third Circuit"
+"http://id.altlaw.org/courts/us/fed/app/4" "United States Court of Appeals for the Fourth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/5" "United States Court of Appeals for the Fifth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/6" "United States Court of Appeals for the Sixth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/7" "United States Court of Appeals for the Seventh Circuit"
+"http://id.altlaw.org/courts/us/fed/app/8" "United States Court of Appeals for the Eighth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/9" "United States Court of Appeals for the Ninth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/10" "United States Court of Appeals for the Tenth Circuit"
+"http://id.altlaw.org/courts/us/fed/app/11" "United States Court of Appeals for the Eleventh Circuit"
+"http://id.altlaw.org/courts/us/fed/app/fed" "United States Court of Appeals for the Federal Circuit"
+"http://id.altlaw.org/courts/us/fed/app/dc" "United States Court of Appeals for the District of Columbia Circuit"
+"http://id.altlaw.org/courts/us/fed/app/af" "United States Court of Appeals for the Armed Forces"
+"http://id.altlaw.org/courts/us/fed/eca" "Emergency Court of Appeals"
+"http://id.altlaw.org/courts/us/fed/teca" "Temporary Emergency Court of Appeals"
+"http://id.altlaw.org/courts/us/fed/customs-patents" "United States Court of Customs and Patent Appeals"
+"http://id.altlaw.org/courts/us/fed/court-of-claims" "United States Court of Claims"
+"http://id.altlaw.org/courts/us/fed/claims" "United States Court of Federal Claims"
+})
