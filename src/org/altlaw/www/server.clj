aeafc371c345c4fa9ff173262ab050f6d637ea79
Stuart Sierra <ssierr@law.columbia.edu>
2009-03-31 15:58:53 -0400

    www/server.clj: added from v4

--- src/org/altlaw/www/server.clj
+++ src/org/altlaw/www/server.clj
@@ -1,10 +1,10 @@
 (ns org.altlaw.www.server
   (:gen-class)
   (:require [org.altlaw.util.context :as context]
-            [org.altlaw.rest.component :as component]
+            [org.altlaw.util.restlet :as component]
             org.altlaw.www.application
-            org.altlaw.rest.status_service_dev
-            org.altlaw.rest.status_service_public))
+            org.altlaw.util.restlet.status_service_dev
+            org.altlaw.www.status_service_public))
 
 (defn setup-logging [component]
   (let [service (.getLogService component)]
@@ -14,8 +14,8 @@
   (let [app (new org.altlaw.www.application)
         port (if (= (context/altlaw-env) "production") 80 8080)]
     (if (= (context/altlaw-env) "production")
-      (.setStatusService app (new org.altlaw.rest.status_service_public))
-      (.setStatusService app (new org.altlaw.rest.status_service_dev)))
+      (.setStatusService app (new org.altlaw.www.status_service_public))
+      (.setStatusService app (new org.altlaw.util.restlet.status_service_dev)))
     (println "Starting www server at port" port "in"
              (context/altlaw-env) "environment.")
     (let [component (component/make-component port app)]
