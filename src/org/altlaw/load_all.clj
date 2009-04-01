0d11f6baa2de531012d3731c633a36c1905a38d6
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-01 11:13:22 -0400

    load_all.clj: added jobs namespaces

--- src/org/altlaw/load_all.clj
+++ src/org/altlaw/load_all.clj
@@ -2,6 +2,23 @@
   (:require 
    org.altlaw.internal.application
 
+   org.altlaw.jobs.distindex
+   org.altlaw.jobs.genhtml
+   org.altlaw.jobs.getindexes
+   org.altlaw.jobs.graph
+   org.altlaw.jobs.graph-map
+   org.altlaw.jobs.link.linkincites
+   org.altlaw.jobs.link.linkincites-mapred
+   org.altlaw.jobs.link.linkoutcites
+   org.altlaw.jobs.link.linkoutcites-mapred
+   org.altlaw.jobs.link.mergedocs
+   org.altlaw.jobs.link.mergedocs-mapred
+   org.altlaw.jobs.procfiles.altcrawl
+   org.altlaw.jobs.procfiles.ohm1
+   org.altlaw.jobs.procfiles.ohm1-map
+   org.altlaw.jobs.procfiles.profed
+   org.altlaw.jobs.procfiles.profed-map
+
    org.altlaw.test.internal
    org.altlaw.test.internal.idserver.impl
    org.altlaw.test.internal.idserver.client
