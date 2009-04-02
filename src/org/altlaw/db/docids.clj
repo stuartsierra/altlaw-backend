2439473f5462710f9f604745237a5151af6d2aaa
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-02 18:53:30 -0400

    db/docids.clj: added max-docid

--- src/org/altlaw/db/docids.clj
+++ src/org/altlaw/db/docids.clj
@@ -1,6 +1,8 @@
 (ns org.altlaw.db.docids
   (:require [org.altlaw.util.simpledb :as db]))
 
+;;; PRIVATE
+
 (def #^{:private true} *domain* "Docids")
 
 (defn- make-db-key [collection-name collection-key]
@@ -8,6 +10,13 @@
     ;; Ensure the key fits within SimpleDB's 1024-byte limit
     (if (< (count s) 1024)
       s (subs s 0 1024))))
+
+(def #^{:private true} *number-format* "%09d")
+
+(def #^{:private true} *base-number-string*
+     (format *number-format* 10000))
+
+;;; PUBLIC
 
 (defn get-docid [collection key]
   (when-let [string (get (db/get-attrs *domain*
@@ -21,8 +30,16 @@
                 {:collection collection
                  :key key
                  ;; Add leading zeros for lexicographic ordering
-                 :docid (format "%09d" docid)}))
+                 :docid (format *number-format* docid)}))
 
 (defn delete-docid [collection key]
   (db/delete-item *domain* (make-db-key collection key)))
+
+(defn max-docid []
+  (let [result (db/select-items
+                *domain*
+                (str "select docid from Docids where docid > "
+                     \' *base-number-string* \'
+                     " order by docid desc limit 1"))]
+    (Integer/parseInt (get (first (vals result)) "docid"))))
 
