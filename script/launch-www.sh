7206c03c148d1e8ce19e7371e0df93bf4d59c9c9
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-07 11:45:56 -0400

    script/: added scripts for launching & preparing WWW instance on EC2

---
+++ script/launch-www.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+S3_BUCKET=altlaw.org
+AMI_NAME=www
+HADOOP_VERSION=0.18.4-dev
+
+# Location of EC2 keys.  The default setting is probably OK if you set
+# up EC2 following the Amazon Getting Started guide.
+EC2_KEYDIR=`dirname "$EC2_PRIVATE_KEY"`
+
+# The EC2 key name used to launch instances.  The default is the value
+# used in the Amazon Getting Started guide.
+KEY_NAME=stuart-altlaw
+
+# Where your EC2 private key is stored (created when following the
+# Amazon Getting Started guide).  You need to change this if you don't
+# store this with your other EC2 keys.
+PRIVATE_KEY_PATH=`echo "$EC2_KEYDIR"/"id_rsa-$KEY_NAME"`
+
+# SSH options used when connecting to EC2 instances.
+SSH_OPTS=`echo -i "$PRIVATE_KEY_PATH" -o StrictHostKeyChecking=no -o ServerAliveInterval=30`
+
+# The AMI image ID.
+AMI_IMAGE=`ec2-describe-images -a | grep $S3_BUCKET | grep $AMI_NAME | grep available | awk '{print $2}'`
+
+
+echo "Launching instance with image $AMI_IMAGE"
+INSTANCE=`ec2-run-instances $AMI_IMAGE | grep INSTANCE | awk '{print $2}'`
+
+echo "Waiting for instance $INSTANCE to boot ..."
+while true; do
+  printf "."
+  # get private dns
+  HOST=`ec2-describe-instances $INSTANCE | grep running | awk '{print $4}'`
+  if [ ! -z $HOST ]; then
+    echo "Started as $HOST"
+    break;
+  fi
+  sleep 1
+done
+
+
+cat > /tmp/remote-prepare-www.sh <<EOF
+#!/bin/sh
+
+chmod 0600 /root/.awssecret
+
+# install AltLaw
+cd /mnt
+git clone --depth 1 git://github.com/lawcommons/altlaw-backend.git
+cd altlaw-backend
+ant
+ant gen.static
+
+# install Hadoop
+cd /mnt
+aws get altlaw.org/v4/hadoop-for-www.tar.gz hadoop.tar.gz
+tar xzf hadoop.tar.gz
+# Find the untarred Hadoop dir, rename it:
+find . -maxdepth 1 -type d -name 'hadoop-*' -exec mv '{}' hadoop \;
+
+# Generate a local SSH key, so Hadoop local config works:
+ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
+cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+
+# Prepare local Hadoop namenode, for s3dfs access:
+cd /mnt/hadoop
+bin/hadoop namenode -format
+
+EOF
+
+
+# Copy .awssecret file:
+scp $SSH_OPTS ~/.awssecret root@$HOST:
+
+# Copy scripts:
+scp $SSH_OPTS /tmp/remote-prepare-www.sh root@$HOST:
+
+# Run the startup script:
+ssh $SSH_OPTS root@$HOST "sh remote-prepare-www.sh"
+
+# Login
+ssh $SSH_OPTS root@$HOST
+
+
---
+++ script/remote/remote-env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source `dirname $0`/../env.sh
+
+ALTLAW_HOME=/mnt/altlaw-backend
+HADOOP_HOME=/mnt/hadoop
