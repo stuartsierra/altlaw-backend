58f0799a92093f2367afa30905515f57053c45a6
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-07 17:39:28 -0400

    script/env.sh: use ALTLAW_HOME instead of HERE

--- script/env.sh
+++ script/env.sh
@@ -19,7 +19,7 @@ fi
 CLASSPATH="$ALTLAW_HOME/src:$ALTLAW_HOME/build/classes:$ALTLAW_HOME/build/clj_classes:$ALTLAW_HOME/lib/*"
 
 JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -Xmx1024m"
-JAVA_OPTS="$JAVA_OPTS -Dorg.altlaw.home=$HERE"
+JAVA_OPTS="$JAVA_OPTS -Dorg.altlaw.home=$ALTLAW_HOME"
 JAVA_OPTS="$JAVA_OPTS -Dorg.altlaw.env=$ALTLAW_ENV"
 JAVA_OPTS="$JAVA_OPTS -Dclojure.compile.path=build/clj_classes"
 
