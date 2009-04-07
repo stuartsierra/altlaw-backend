01a928e16174531b84e19cda35fd74387630f6f0
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-07 12:07:07 -0400

    script/remote/fetch-index.sh: fixed paths

--- script/remote/fetch-index.sh
+++ script/remote/fetch-index.sh
@@ -2,11 +2,13 @@
 
 source `dirname $0`/remote-env.sh
 
+INDEX_DIR=$ALTLAW_HOME/var/solr/data/index
+
 # Copy Lucene index
-mkdir -p $ALTLAW_HOME/var/solr/data/index
-rm -f $ALTLAW_HOME/var/solr/data/index/*
+mkdir -p $INDEX_DIR
+rm -f $INDEX_DIR/*
 cd $HADOOP_HOME
 bin/start-dfs.sh
-bin/hadoop fs -get "s3://s3dfs.altlaw.org/v4/merged/*" /mnt/altlaw-backend/index/data/index/
+bin/hadoop fs -get "s3://s3dfs.altlaw.org/v4/merged/*" $INDEX_DIR/
 bin/stop-dfs.sh
 
