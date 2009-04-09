334b79a0eebf673893ba036fd695a28205fa2484
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-09 11:43:47 -0400

    script/env.sh: do not assume ALTLAW_HOME dir

--- script/env.sh
+++ script/env.sh
@@ -6,22 +6,34 @@ then
     exit 1;
 fi
 
+if [ -z "$ALTLAW_HOME" ]
+then
+    echo "Error: ALTLAW_HOME not set."
+    exit 1
+fi
+
+if [ -z "$HADOOP_HOME" ]
+then
+    echo "Error: HADOOP_HOME not set."
+    exit 1
+fi
+
 if [ -z "$ALTLAW_ENV" ]
 then
+    echo "Assuming ALTLAW_ENV=development"
     ALTLAW_ENV="development"
 fi
 
-if [ -z "$ALTLAW_HOME" ]
-then
-    ALTLAW_HOME=`dirname $0`/..
-fi
 
-CLASSPATH="$ALTLAW_HOME/src:$ALTLAW_HOME/build/classes:$ALTLAW_HOME/build/clj_classes:$ALTLAW_HOME/lib/*"
+CP="$ALTLAW_HOME/src"
+CP="$CP:$ALTLAW_HOME/build/classes"
+CP="$CP:$ALTLAW_HOME/build/clj_classes"
+CP="$CP:$ALTLAW_HOME/lib/*"
 
 JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC -Xmx1024m"
 JAVA_OPTS="$JAVA_OPTS -Dorg.altlaw.home=$ALTLAW_HOME"
 JAVA_OPTS="$JAVA_OPTS -Dorg.altlaw.env=$ALTLAW_ENV"
-JAVA_OPTS="$JAVA_OPTS -Dclojure.compile.path=build/clj_classes"
+JAVA_OPTS="$JAVA_OPTS -Dclojure.compile.path=$ALTLAW_HOME/build/clj_classes"e
 
-JAVA="$JAVA_HOME/bin/java -cp $CLASSPATH $JAVA_OPTS"
+JAVA="$JAVA_HOME/bin/java -cp $CP $JAVA_OPTS"
 
