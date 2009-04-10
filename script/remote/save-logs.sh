f434e304a583b6709998f2a2519e532003db185a
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-10 13:51:52 -0400

    save-logs.sh: fix file names in aws put

--- script/remote/save-logs.sh
+++ script/remote/save-logs.sh
@@ -3,14 +3,15 @@
 source `dirname $0`/../env.sh
 
 DATE=`date +%Y%m%d%H%M%S`
-LOGFILE="/mnt/www.access.log.$DATE.gz"
-LOGDIR=$ALTLAW_HOME/var/log/production
-SAVELOGS_DIR=/mnt/savelogs
+LOGFILE_NAME="www.access.log.$DATE.gz"
+LOGFILE_PATH="/mnt/$LOGFILE_NAME"
+LOGDIR="$ALTLAW_HOME/var/log/production"
+SAVELOGS_DIR="/mnt/savelogs"
 
 # Copy logs
 mkdir -p $SAVELOGS_DIR
 rm -f $SAVELOGS_DIR/*
 mv $LOGDIR/www.access.log.* $SAVELOGS_DIR/
-cat $SAVELOGS_DIR/* | gzip -9 > $LOGFILE
-aws put altlaw.org/v4/log/www/$LOGFILE $LOGFILE
-rm $LOGFILE
+cat $SAVELOGS_DIR/* | gzip -9 > $LOGFILE_PATH
+aws put altlaw.org/v4/log/www/$LOGFILE_NAME $LOGFILE_PATH
+rm $LOGFILE_PATH
