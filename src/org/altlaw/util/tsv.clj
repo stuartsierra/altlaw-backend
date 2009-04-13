00d38784b7f31498b4b7efeaf02a86804fb93996
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-13 11:04:26 -0400

    util/tsv.clj: added read-tsv-stream

--- src/org/altlaw/util/tsv.clj
+++ src/org/altlaw/util/tsv.clj
@@ -5,16 +5,28 @@
                     FileInputStream)
            (java.util.zip GZIPInputStream)))
 
+(defn read-tsv-stream
+  "Reads a gzip-compressed binary stream of a tab-separated-value file
+  with two columns.  Returns a map of string=>string."
+  [#^InputStream stream]
+  (with-open [reader (BufferedReader.
+                      (InputStreamReader.
+                       (GZIPInputStream. stream) "UTF-8"))]
+    (reduce (fn [map #^String line]
+              (let [[#^String key value] (.split line "\t")]
+                (assoc map key value)))
+            {} (line-seq stream))))
+
+(defn write-tsv-stream
+  "Writes a gzip-compressed binary stream of a tab-separated-value
+  file containing two columns, drawing data from the given map."
+  [stream map]
+  )
+
 (defn load-tsv-map
   "Reads a gzip-compressed tab-separated-value file with two columns.
   Returns a map of string=>string."
   [file]
-  (with-open [stream (BufferedReader.
-                      (InputStreamReader.
-                       (GZIPInputStream.
-                        (FileInputStream. file))))]
-      (reduce (fn [map #^String line]
-                  (let [[#^String key value] (.split line "\t")]
-                    (assoc map key value)))
-              {} (line-seq stream))))
+  (with-open [stream (FileInputStream. file)]
+    (read-tsv-stream stream)))
 
