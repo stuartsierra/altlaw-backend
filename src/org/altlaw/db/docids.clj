fa7ad8a563b2ff63ebbe5f150696d0b69e4301ce
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-13 11:05:11 -0400

    db/docids.clj: use S3 instead of SimpleDB

--- src/org/altlaw/db/docids.clj
+++ src/org/altlaw/db/docids.clj
@@ -1,45 +1,23 @@
-(ns org.altlaw.db.docids
-  (:require [org.altlaw.util.simpledb :as db]))
+(ns org.altlaw.util.docids)
 
-;;; PRIVATE
+(def #^{:private true} *bucket* "altlaw.org")
 
-(def #^{:private true} *domain* "Docids")
+(defn- path [collection]
+  (str "v5/docids/" collection ".tsv.gz"))
 
-(defn- make-db-key [collection-name collection-key]
-  (let [s (str collection-name ":" collection-key)]
-    ;; Ensure the key fits within SimpleDB's 1024-byte limit
-    (if (< (count s) 1024)
-      s (subs s 0 1024))))
+(defn load-docids [collection]
+  (read-tsv-stream (get-object-stream *bucket* (path collection))))
 
-(def #^{:private true} *number-format* "%010d")
+(defn save-docids [collection])
 
-(def #^{:private true} *base-number-string*
-     (format *number-format* 10000))
-
-;;; PUBLIC
+(def get-docid-map (memoize (load-docids)))
 
 (defn get-docid [collection key]
-  (when-let [string (get (db/get-attrs *domain*
-                                     (make-db-key collection key))
-                       "docid")]
-    ;; Integer/parseInt ignores leading zeros
-    (Integer/parseInt string)))
+  (get @(get-docid-map collection) key))
 
-(defn put-docid [collection key docid]
-  (db/put-attrs *domain* (make-db-key collection key)
-                {:collection collection
-                 :key key
-                 ;; Add leading zeros for lexicographic ordering
-                 :docid (format *number-format* docid)}))
+(defn set-docid [collection keys docid]
+  (dosync (alter @(get-docid-map collection)
+                 merge (zipmap keys (repeat docid)))))
 
-(defn delete-docid [collection key]
-  (db/delete-item *domain* (make-db-key collection key)))
-
-(defn max-docid []
-  (let [result (db/select-items
-                *domain*
-                (str "select docid from Docids where docid > "
-                     \' *base-number-string* \'
-                     " order by docid desc limit 1"))]
-    (Integer/parseInt (get (first (vals result)) "docid"))))
+(defn request-docid [collection keys])
 
