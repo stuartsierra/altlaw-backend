f1b729c1ef74e46f17ce114840932e3d15b377b0
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-14 11:35:56 -0400

    DocResource.clj: handle S3 NoSuchKey errors

--- src/org/altlaw/www/DocResource.clj
+++ src/org/altlaw/www/DocResource.clj
@@ -1,6 +1,7 @@
 (ns org.altlaw.www.DocResource
   (:gen-class :extends org.restlet.resource.Resource)
-  (:require [org.altlaw.db.content :as content])
+  (:require [org.altlaw.db.content :as content]
+            [clojure.contrib.stacktrace :as stack])
   (:import (org.restlet.resource Variant StringRepresentation ResourceException)
            (org.restlet.data MediaType Language CharacterSet Reference Status)))
 
@@ -12,8 +13,15 @@
   [(Variant. MediaType/TEXT_HTML)])
 
 (defn -represent [this variant]
-  (let [path (.. this getRequest getOriginalRef (getPath true))]
-    (StringRepresentation.
-     ;; Truncate the initial "/" from path:
-     (content/get-page (str (.substring path 1) ".html"))
-     MediaType/TEXT_HTML Language/ENGLISH_US CharacterSet/UTF_8)))
+  (try
+   (let [path (.. this getRequest getOriginalRef (getPath true))
+         ;; Truncate the initial "/" from path:
+         content (content/get-page (str (.substring path 1) ".html"))]
+     (StringRepresentation.
+      content
+      MediaType/TEXT_HTML Language/ENGLISH_US CharacterSet/UTF_8))
+   (catch Exception e
+     (if (and (instance? org.jets3t.service.S3ServiceException
+                         (stack/root-cause e)))
+       (throw (ResourceException. Status/CLIENT_ERROR_NOT_FOUND e))
+       (throw e)))))
