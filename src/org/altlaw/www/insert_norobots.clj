cd3c14e1ac18555cbafae582d562591dd4f83a24
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-15 12:54:14 -0400

    www/insert_norobots.txt: added more reporting

--- src/org/altlaw/www/insert_norobots.clj
+++ src/org/altlaw/www/insert_norobots.clj
@@ -4,10 +4,14 @@
 (def *meta-tag* "<meta name=\"robots\" content=\"noindex\" />")
 
 (defn insert-norobots-tag [docid]
-  (let [path (str "v1/cases/" docid ".html")
-        original (content/get-page path)]
-    (if (.contains original *meta-tag*)
-      :no-change-needed
-      (let [modified (.replaceFirst original "</head>" (str *meta-tag* "</head>"))]
-        (content/put-page-string path modified "text/html")
-        :changed))))
+  (println "Checking norobots on" docid)
+  (try
+   (let [path (str "v1/cases/" docid ".html")
+         original (content/get-page path)]
+     (if (.contains original *meta-tag*)
+       (println "No changed needed for" docid)
+       (let [modified (.replaceFirst original "</head>" (str *meta-tag* "</head>"))]
+         (content/put-page-string path modified "text/html")
+         (println "Changed " docid))))
+   (catch Exception e
+     (println "Exception" e "on" docid))))
