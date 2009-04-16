ce54f0e5bb20be629df508bcc5bc3a67379a0c04
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-16 17:40:24 -0400

    extract/pdf.clj: move pdf-to-html code from ohm1 jobs to here

---
+++ src/org/altlaw/extract/pdf.clj
@@ -0,0 +1,22 @@
+(ns org.altlaw.extract.pdf
+  (:require [clojure.contrib.singleton :as sing]
+            [org.altlaw.util.log :as log])
+  (:import (org.altlaw.util RunProgramOnFile)
+           (org.apache.commons.exec ExecuteException)
+           (java.io File)))
+
+(def *pdf-to-html-executable* (File. "/usr/local/bin/altlaw_parse_pdf"))
+
+(def #^{:private true} get-runner
+     (sing/per-thread-singleton
+      (fn []
+        (doto (RunProgramOnFile.)
+          (.setExecutable *pdf-to-html-executable*)))))
+
+
+(defn pdf-to-html [filename bytes]
+  (try 
+   (String. (.exec (get-runner) bytes))
+   (catch ExecuteException e
+     (log/warn "altlaw_parse_pdf failed on " filename ": " e)
+     nil)))
