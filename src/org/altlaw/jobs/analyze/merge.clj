d8a6bdb980f57bf046b5639b96abdbf83dfdf61a
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-17 15:27:13 -0400

    analyze/merge.clj: added job to merge docs and cite fields

---
+++ src/org/altlaw/jobs/analyze/merge.clj
@@ -0,0 +1,31 @@
+(ns org.altlaw.jobs.analyze.merge
+  (:refer clojure.set)
+  (:require [org.altlaw.util.hadoop :as h]
+            [org.altlaw.util.log :as log]
+            [org.altlaw.util.merge-fields :as merge]))
+
+(h/setup-mapreduce)
+
+
+(defn my-reduce [docid documents]
+  [[docid (merge/merge-fields documents)]])
+
+;;; METHOD IMPLEMENTATIONS
+
+(def reducer-reduce (partial h/standard-reduce my-reduce))
+
+(defn tool-run [this args] []
+  (let [job (h/default-jobconf this)
+        outpath (Path. (h/job-path :analyze :merge))
+        hdfs (FileSystem/get job)]
+    (.setJobName job "merge")
+    (FileInputFormat/setInputPaths job (str (h/job-path :pre :profed)
+                                            "," (h/job-path :pre :ohm1)
+                                            "," (h/job-path :analyze :cite_fields)))
+    (FileOutputFormat/setOutputPath job outpath)
+    (.delete hdfs outpath true)
+    (.setMapperClass job IdentityMapper)
+    (.setReducerClass job org.altlaw.jobs.analyze.merge_reducer)
+    (JobClient/runJob job))
+  0)
+
