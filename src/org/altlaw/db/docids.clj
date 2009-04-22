85aed6fb4e327fda226b7e71965b2f2b1955f5fd
Stuart Sierra <ssierr@law.columbia.edu>
2009-04-22 14:41:06 -0400

    db/docids.clj: database of docids with db.data

--- src/org/altlaw/db/docids.clj
+++ src/org/altlaw/db/docids.clj
@@ -1,23 +1,24 @@
-(ns org.altlaw.util.docids)
+(ns org.altlaw.db.docids
+  (:require [org.altlaw.db.data :as data]))
 
 (def #^{:private true} *bucket* "altlaw.org")
 
-(defn- path [collection]
-  (str "v5/docids/" collection ".tsv.gz"))
+(defn create-docids [collection]
+  (data/create-data (str "docids/" collection) {}))
 
-(defn load-docids [collection]
-  (read-tsv-stream (get-object-stream *bucket* (path collection))))
+(defn get-docids [collection]
+  (data/get-data (str "docids/" collection)))
 
-(defn save-docids [collection])
-
-(def get-docid-map (memoize (load-docids)))
+(defn save-docids [collection]
+  (data/save-data (str "docids/" collection)))
 
 (defn get-docid [collection key]
-  (get @(get-docid-map collection) key))
+  (get @(get-docids collection key)))
 
 (defn set-docid [collection keys docid]
-  (dosync (alter @(get-docid-map collection)
-                 merge (zipmap keys (repeat docid)))))
-
-(defn request-docid [collection keys])
+  (assert (coll? keys))
+  (assert (every? string? keys))
+  (assert (integer? docid))
+  (dosync (apply alter (get-docids collection)
+                 assoc (zipmap keys (repeat docid)))))
 
