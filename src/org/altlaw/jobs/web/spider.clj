cbcee2d4a6899d62bed7a84f4f14ac61e8529792
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-01 17:11:11 -0400

    jobs/web/spider.clj: generate requests for scrapable URIs

---
+++ src/org/altlaw/jobs/web/spider.clj
@@ -0,0 +1,17 @@
+(ns org.altlaw.jobs.web.spider
+  (:require [org.altlaw.util.hadoop :as h]
+            [org.altlaw.extract.scrape.handler :as handler]
+            [clojure.contrib.duck-streams :as duck]))
+
+(h/setup-mapreduce)
+
+(defn tool-run [this args]
+  (let [job (h/default-jobconf this)
+        outpath (Path. (h/job-path :web :request))
+        hdfs (FileSystem/get job)]
+    (.delete hdfs outpath true)
+    (with-open [hdfs-stream (.create hdfs (Path. outpath "part-99999") true)]
+      (binding [*out* (duck/writer (.getWrappedStream hdfs-stream))]
+        (doseq [r (handler/all-requests)]
+          (prn r))))
+    0))
