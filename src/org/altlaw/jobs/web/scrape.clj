a71d846098a093d9a3f0d7b7dfc5cb1f515867c8
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-01 14:58:21 -0400

    web/scrape.clj: added web scrape job

---
+++ src/org/altlaw/jobs/web/scrape.clj
@@ -0,0 +1,59 @@
+(ns org.altlaw.jobs.web.scrape
+  (:require [org.altlaw.extract.scrape.handler :as handler]
+            [org.altlaw.util.log :as log]
+            [org.altlaw.util.files :as files]
+            [org.altlaw.util.hadoop :as h])
+  (:import (java.net URI)
+           (org.apache.commons.codec.binary Base64)))
+
+(h/setup-mapreduce)
+
+(defn decode-response-body [download]
+  (assoc download :response_body_bytes
+         (Base64/decodeBase64
+          (.getBytes (:response_body_base64 download) "UTF-8"))))
+
+(defn mime-type [download]
+  (files/guess-mime-type-by-content (:response_body_bytes download))  )
+
+(defn handle-scraper-result [result]
+  (cond
+    (contains? result :exception)
+    (do (log/warn (pr-str result))
+        (h/counter "Scraper results" "exceptions")
+        [:exception result])
+
+    (contains? result :court)
+    (do (h/counter "Scraper results" "found documents")
+        [:document result])
+
+    :else
+    (do (h/counter "Scraper results" "UNRECOGNIZED")
+        (log/warn "Unrecognized scraper result: " (pr-str result))
+        [:unrecognized result])))
+
+(defn my-map [line-number download]
+  (let [download (decode-response-body download)
+        type (mime-type download)]
+    (h/counter "MIME Types" type)
+    (when (= type "text/html")
+      (map handle-scraper-result
+           (handler/run-scrapers download)))))
+
+(def mapper-map (partial h/standard-map my-map))
+
+(defn tool-run [this args]
+  (let [job (h/default-jobconf this)
+        inpath (Path. (h/job-path :web :crawl))
+        outpath (Path. (h/job-path :web :scrape))
+        hdfs (FileSystem/get job)]
+    (.delete hdfs outpath true)
+    (FileInputFormat/setInputPaths job (str inpath))
+    (FileOutputFormat/setOutputPath job outpath)
+    (.setMapperClass job org.altlaw.jobs.web.scrape_mapper)
+    ;; (.setReducerClass job org.altlaw.jobs.web.scrape_reducer)
+    (.setNumReduceTasks job 0)
+    (.setInputFormat job TextInputFormat)
+    (.setJobName job "web.scrape")
+    (JobClient/runJob job))
+  0)
--- src/org/altlaw/load_all.clj
+++ src/org/altlaw/load_all.clj
@@ -2,6 +2,7 @@
   (:require 
    ;; org.altlaw.jobs.pre.altcrawl
    org.altlaw.jobs.web.crawl
+   org.altlaw.jobs.web.scrape
    org.altlaw.jobs.pre.ohm1
    org.altlaw.jobs.pre.profed
    org.altlaw.jobs.analyze.graph
