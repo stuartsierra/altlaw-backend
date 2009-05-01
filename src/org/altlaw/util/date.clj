c31179eb1aa75c871f7af4658ba174cb493a7133
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-01 17:11:48 -0400

    util/date.clj: added filename-timestamp

--- src/org/altlaw/util/date.clj
+++ src/org/altlaw/util/date.clj
@@ -1,12 +1,16 @@
 (ns org.altlaw.util.date
   (:use [clojure.contrib.test-is :only (with-test is)])
-  (:import (java.text SimpleDateFormat FieldPosition ParsePosition)))
+  (:import (java.text SimpleDateFormat FieldPosition ParsePosition)
+           (java.util Date)))
 
 (def #^{:private true} *iso-date-format*
      (SimpleDateFormat. "yyyy-MM-dd"))
 
 (def #^{:private true} *long-date-format*
      (SimpleDateFormat. "MMMM d, yyyy"))
+
+(def #^{:private true} *datetime-filename-format*
+     (SimpleDateFormat. "yyyyMMddHHmmss"))
 
 (with-test
     (defn format-long-date
@@ -21,3 +25,5 @@
                       (FieldPosition. 0)))))
   (is (= "January 1, 2009" (format-long-date "2009-01-01"))))
 
+(defn filename-timestamp []
+  (.format *datetime-filename-format* (Date.)))
