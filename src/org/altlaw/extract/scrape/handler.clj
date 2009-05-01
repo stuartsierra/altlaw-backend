6caf3251d9385f13559dfdb28de1112984649ad4
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-01 12:09:02 -0400

    extract/scrape/handler.clj: Clojure interface to ScraperHandler

---
+++ src/org/altlaw/extract/scrape/handler.clj
@@ -0,0 +1,24 @@
+(ns org.altlaw.extract.scrape.handler
+  (:require [org.altlaw.util.jruby :as ruby]
+            [clojure.contrib.walk :as walk]
+            [clojure.contrib.singleton :as sing]))
+
+(def scraper-handler
+     (sing/per-thread-singleton
+      (fn []
+        (ruby/eval-jruby "require 'org/altlaw/extract/scrape/scraper_handler'")
+        (ruby/eval-jruby "ScraperHandler.new"))))
+
+(defn run-scrapers [download]
+  (assert (map? download))
+  (assert (contains? download :request_uri))
+  (assert (contains? download :response_body_bytes))
+  (map ruby/convert-jruby
+       (ruby/eval-jruby "$handler.parse(Download.from_map($download))"
+                        {:download (walk/stringify-keys download)
+                         :handler (scraper-handler)})))
+
+(defn all-requests []
+  (map ruby/convert-jruby
+       (ruby/eval-jruby "$handler.all_requests"
+                        {:handler (scraper-handler)})))
