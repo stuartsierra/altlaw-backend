2ff578f87b1f62aa3696a5845abb8f83077f90ae
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-18 16:51:31 -0400

    www/admin.clj: added route for raw log directory

--- src/org/altlaw/www/admin.clj
+++ src/org/altlaw/www/admin.clj
@@ -3,8 +3,9 @@
             org.altlaw.www.admin.NorobotsResource
             org.altlaw.www.admin.MenuResource
             [clojure.contrib.shell-out :as sh])
-  (:import (org.restlet Router Guard)
-           (org.restlet.data ChallengeScheme)))
+  (:import (org.restlet Router Guard Directory)
+           (org.restlet.data ChallengeScheme)
+           (java.io File)))
 
 (defn- make-admin-guard [context next]
   (doto (Guard. context ChallengeScheme/HTTP_DIGEST
@@ -25,12 +26,18 @@
 (defn- make-command-restlet [context args]
   (make-fn-restlet context (fn [] (sh-in-thread args))))
 
+(defn- logs-uri []
+  (str "file://" (.getAbsolutePath (File. (context/altlaw-home)))
+       "/var/log/" (context/altlaw-env)))
+
 (defn- make-admin-router [context]
   (doto (Router. context)
     (.attach "/norobots" org.altlaw.www.admin.NorobotsResource)
     (.attach "/update-code" (make-command-restlet context ["git" "pull"]))
     (.attach "/recompile" (make-command-restlet context ["ant"]))
     (.attach "/make-content-pages" (make-fn-restlet context org.altlaw.www.content-pages/save-static-pages))
+    (.attach "/logs" (doto (Directory. context (logs-uri))
+                       (.setListingAllowed true)))
     (.attach "" org.altlaw.www.admin.MenuResource)))
 
 (defn make-guarded-admin-router [context]
