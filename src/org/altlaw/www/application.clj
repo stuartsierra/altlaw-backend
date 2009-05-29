122d2c67d7186e28ba5198cae8a6a15bedc90827
Stuart Sierra <ssierr@law.columbia.edu>
2009-05-29 13:45:56 -0400

    application.clj: use FormatFilter around searches

--- src/org/altlaw/www/application.clj
+++ src/org/altlaw/www/application.clj
@@ -5,6 +5,7 @@
   (:require org.altlaw.www.StaticFinder
             org.altlaw.www.CiteFinder
             org.altlaw.www.AllCasesFinder
+            org.altlaw.www.FormatFilter
             org.altlaw.www.SearchResource
             org.altlaw.www.CitationsResource
             org.altlaw.www.AdminNorobotsResource
@@ -38,12 +39,15 @@
 
 (defn -createRoot [this]
   (let [context (.getContext this)
-        static-finder (new org.altlaw.www.StaticFinder context)]
+        static-finder (new org.altlaw.www.StaticFinder context)
+        searcher (new org.altlaw.www.FormatFilter context)]
+    (.setNext searcher org.altlaw.www.SearchResource)
     (doto (Router. context)
       (.attach "/cite/{citation}" (new org.altlaw.www.CiteFinder context))
-      (.attach "/v1/cases" (new org.altlaw.www.AllCasesFinder context))
-      (.attach "/v1/search/{query_type}" org.altlaw.www.SearchResource)
-      (.attach "/v1/search" org.altlaw.www.SearchResource)
+      (.attach "/v1/cases" (new org.altlaw.www.FormatFilter context
+                                (new org.altlaw.www.AllCasesFinder context)))
+      (.attach "/v1/search/{query_type}" searcher)
+      (.attach "/v1/search" searcher)
       (.attach "/v1/cases/{docid}/citations" org.altlaw.www.CitationsResource)
       (.attach "/v1/cases/{docid}" org.altlaw.www.DocResource)
       (.attach "/admin" (admin/make-guarded-admin-router context))
