5e5da4a3611b5af87ca6829240a4a512838d45bf
Stuart Sierra <ssierr@law.columbia.edu>
2009-06-02 13:01:32 -0400

    util/date.clj: factored out parse-iso-date from format-long-date

--- src/org/altlaw/util/date.clj
+++ src/org/altlaw/util/date.clj
@@ -12,19 +12,23 @@
 (def #^{:private true} *datetime-filename-format*
      (SimpleDateFormat. "yyyyMMddHHmmss"))
 
+(defn parse-iso-date [string]
+  (when (seq string)
+    (.parse *iso-date-format* string
+            (ParsePosition. 0))))
+
 (with-test
     (defn format-long-date
       "Converts a String date from YYYY-MM-DD format to 
       'January 1, 2009' format."
       [string]
-      (when (seq string)
-        (str (.format *long-date-format*
-                      (.parse *iso-date-format* string
-                              (ParsePosition. 0))
-                      (StringBuffer.)
-                      (FieldPosition. 0)))))
+      (str (.format *long-date-format*
+                    (parse-iso-date string)
+                    (StringBuffer.)
+                    (FieldPosition. 0))))
   (is (= "January 1, 2009" (format-long-date "2009-01-01")))
   (is (= nil (format-long-date ""))))
 
 (defn filename-timestamp []
   (.format *datetime-filename-format* (Date.)))
+
