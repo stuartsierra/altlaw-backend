0e8e68025c4ae971ed254c9ece83caf525a7f5cf
Stuart Sierra <ssierr@law.columbia.edu>
2009-06-10 16:47:28 -0400

    www/admin.clj: added AdminDocResource

--- src/org/altlaw/www/admin.clj
+++ src/org/altlaw/www/admin.clj
@@ -2,6 +2,7 @@
   (:require [org.altlaw.util.context :as context]
             org.altlaw.www.admin.NorobotsResource
             org.altlaw.www.admin.MenuResource
+            org.altlaw.www.admin.AdminDocResource
             [clojure.contrib.shell-out :as sh])
   (:import (org.restlet Router Guard Directory)
            (org.restlet.data ChallengeScheme)
@@ -23,6 +24,12 @@
 (defn- sh-in-thread [args]
   (apply sh/sh (concat args [:dir (str (context/altlaw-home))])))
 
+(defn- solr-commit-restlet [context]
+  (make-fn-restlet context
+                   (fn [] (.. context getAttributes
+                              (get "org.altlaw.solr.server")
+                              commit))))
+
 (defn- make-command-restlet [context args]
   (make-fn-restlet context (fn [] (sh-in-thread args))))
 
@@ -32,10 +39,13 @@
 
 (defn- make-admin-router [context]
   (doto (Router. context)
+    (.attach "/docs/{docid}" org.altlaw.www.admin.AdminDocResource)
+    (.attach "/docs" org.altlaw.www.admin.AdminDocResource)
     (.attach "/norobots" org.altlaw.www.admin.NorobotsResource)
     (.attach "/update-code" (make-command-restlet context ["git" "pull"]))
     (.attach "/recompile" (make-command-restlet context ["ant"]))
     (.attach "/make-content-pages" (make-fn-restlet context org.altlaw.www.content-pages/save-static-pages))
+    (.attach "/solr-commit" (solr-commit-restlet context))
     (.attach "/logs" (doto (Directory. context (logs-uri))
                        (.setListingAllowed true)))
     (.attach "" org.altlaw.www.admin.MenuResource)))
